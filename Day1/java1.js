9fa39378b2450485183f799b222e3ae10b8dd392
Armankhan13 <arman.9c123@gmail.com>
2026-01-28 23:58:44 +0530

    Initial commit: Adding JavaScript folders

---
+++ Day1/java1.js
@@ -0,0 +1,7 @@
+console.log("Hello")
+console.warn("hi")
+console.error("okk")
+alert("hyy")
+// prompt("hello");
+var name=prompt("hello");
+console.log(name)
---
+++ Day2/java2.js
@@ -0,0 +1,30 @@
+var arr=[1,2,3,4,5];
+var b=arr;
+var cd=[...arr];
+cd.pop();
+var age=9;
+if(age<6)console.log("hi");
+else if(age==9)console.log("ok");
+else{
+    console.log("no");
+}
+for(var i=0;i<5;i++){
+    console.log(i);
+}
+var i=10;
+while(i>=0){
+    console.log(i);
+    i--;
+}
+function abcd(){
+    console.log("hi");
+        console.log("hello");
+    console.log("yo");
+    console.log("ok");
+}
+abcd();
+abcd();
+function abc( d){
+    console.log(12+d);
+}
+abc(8);
